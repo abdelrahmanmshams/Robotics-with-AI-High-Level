f4d3a3d08b88117fe5c3198735eb501c252a2fdb
Abdelrahman Shams <abdelrahmanshams1920@gmail.com>
2026-09-12 17:16:40 +0300

    Add Arduino LED control

---
+++ Lecture_12/arduino_led.ino
@@ -0,0 +1,24 @@
+int ledPin = 13;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    if (Serial.available())
+    {
+        char data = Serial.read();
+
+        if (data == '1')
+        {
+            digitalWrite(ledPin, HIGH);
+        }
+        else if (data == '0')
+        {
+            digitalWrite(ledPin, LOW);
+        }
+    }
+}
